1d09d8220c13f63953eeff8c143987e4b482b0d0
OroChippw <OroChippw@163.com>
2023-10-30 11:03:01 +0800

    feat:func Add Double lines adjust effective skipping

---
+++ python/run_FaceRegister.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+import os
+import cv2
+import time
+import argparse
+import os.path as osp
+
+class FaceRegister():
+    def __init__(self , face_db_dir=None) -> None:
+        self.face_db_dir = face_db_dir
+        
+        pass
+    
+    def _frame_register():
+        pass
+    
+def main():
+    parser = argparse.ArgumentParser()
+    
+    args = parser.parse_args()
+    
+    return
+
+if __name__ == "__main__":
+    main()
+    
+    
